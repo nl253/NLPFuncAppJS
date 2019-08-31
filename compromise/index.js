5bb853373948ac25fafad69320bb841b9c1695cc
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-08-31 16:28:19 +0100

    Convert output to array (always)

--- compromise/index.js
+++ compromise/index.js
@@ -21,7 +21,7 @@ module.exports = async function (context, req) {
   context.log('[Node.js HTTP %s FuncApp] %s', basename(__dirname), req.originalUrl);
 
   if (req.body && req.body.text && req.body.type) {
-    ok.body = JSON.stringify(nlp(req.body.text)[req.body.type]().data());
+    ok.body = JSON.stringify(nlp(req.body.text)[req.body.type]().out('array'));
     context.res = ok;
   } else {
     context.res = bad;
