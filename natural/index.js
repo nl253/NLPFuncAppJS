f81e6394abc6e7bf51699cd31f36b037d91a852c
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-08-31 20:46:26 +0100

    Allow to choose tokenizer & stemmer

--- natural/index.js
+++ natural/index.js
@@ -1,5 +1,9 @@
 const { basename } = require('path');
 const {
+  TreebankWordTokenizer,
+  LancasterStemmer,
+  OrthographyTokenizer,
+  WordPunctTokenizer,
   DiceCoefficient,
   LevenshteinDistance,
   DamerauLevenshteinDistance,
@@ -21,10 +25,28 @@ function spellcheck(word) {
 
 /**
  * @param {string} txt
+ * @param {'WordPunctTokenizer'|'WordTokenizer'|'TreebankWordTokenizer'|'WordTokenizer'|'OrthographyTokenizer'} [tokenizer]
  * @return {string[]}
  */
-function tokenize(txt) {
-  return new WordTokenizer().tokenize(txt);
+function tokenize(txt, tokenizer = 'WordTokenizer') {
+  switch (tokenizer) {
+    case 'OrthographyTokenizer': return new OrthographyTokenizer().tokenize(txt);
+    case 'WordTokenizer': return new WordTokenizer().tokenize(txt);
+    case 'WordPunctTokenizer': return new WordPunctTokenizer().tokenize(txt);
+    case 'TreebankWordTokenizer': return new TreebankWordTokenizer().tokenize(txt);
+  }
+}
+
+/**
+ *
+ * @param {string} word
+ * @param {'PorterStemmer'|'LancasterStemmer'} [stemmer]
+ */
+function stem(word, stemmer = 'PorterStemmer') {
+  switch (stemmer) {
+    case 'LancasterStemmer': return LancasterStemmer.stem(word);
+    case 'PorterStemmer': return PorterStemmer.stem(word);
+  }
 }
 
 /**
@@ -37,17 +59,21 @@ function sentiment(txt) {
 
 /**
  * @param {string} txt
+ * @param {'WordPunctTokenizer'|'WordTokenizer'|'TreebankWordTokenizer'|'WordTokenizer'|'OrthographyTokenizer'} [tokenizer]
+ * @param {'PorterStemmer'|'LancasterStemmer'} [stemmer]
  * @return {string[]}
  */
-function tokenizeAndStem(txt) {
-  const stemmer = new PorterStemmer();
-  return tokenize(txt).map(w => stemmer.stem(w));
+function tokenizeAndStem(txt, tokenizer = 'WordTokenizer', stemmer = 'PorterStemmer') {
+  const Stemmer = stemmer === 'PorterStemmer'
+    ? new PorterStemmer()
+    : new LancasterStemmer();
+  return tokenize(txt).map(w => Stemmer.stem(w));
 }
 
 /**
  * @param {string} s1
  * @param {string} s2
- * @param {'LevenshteinDistance'|'DamerauLevenshteinDistance'|'JaroWinklerDistance'|'DiceCoefficient'} metric
+ * @param {'LevenshteinDistance'|'DamerauLevenshteinDistance'|'JaroWinklerDistance'|'DiceCoefficient'} [metric]
  * @return {number}
  */
 function distance(s1, s2, metric = 'LevenshteinDistance') {
@@ -68,16 +94,6 @@ function match(s1, s2) {
   return LevenshteinDistance(s1, s2, {search: true});
 }
 
-const actions = {
-  tokenize,
-  sentiment,
-  tokenizeAndStem,
-  distance,
-  spellcheck,
-  match,
-};
-const actionNames = new Set(Object.keys(actions));
-
 const ok = {
   status: 200,
   headers: {
@@ -90,49 +106,51 @@ const bad = {
   headers: {
     'Content-Type': 'text/plain',
   },
-  body: `Please pass text in the request body and choose from actions: {${[...actionNames].join(', ')}}`,
+  body: `Please pass text in the request body and choose from actions`,
 };
 
 /**
  * @param {{res: *, log: Function<...*, void>}} context
- * @param {{originalUrl: string, body: {action: ('tokenize'|'match'|'tokenizeAndStem'|'distance'|'natural'|'spellcheck'|'sentiment'), text: string, text1: string, text2: string, metric: ('LevenshteinDistance'|'DamerauLevenshteinDistance'|'JaroWinklerDistance'|'DiceCoefficient')}}} req
+ * @param {{originalUrl: string, body: {action: ('stem'|'tokenize'|'match'|'tokenizeAndStem'|'distance'|'natural'|'spellcheck'|'sentiment'), text: string, text1: string, text2: string, metric: ('LevenshteinDistance'|'DamerauLevenshteinDistance'|'JaroWinklerDistance'|'DiceCoefficient'), stemmer: ('PorterStemmer'|'LancasterStemmer'), tokenizer: ('WordPunctTokenizer'|'WordTokenizer'|'TreebankWordTokenizer'|'WordTokenizer'|'OrthographyTokenizer')}}} req
  * @return {Promise<void>}
  */
 module.exports = async function (context, req) {
   context.log('[Node.js HTTP %s FuncApp] %s', basename(__dirname), req.originalUrl);
 
-  if (req.body && req.body.action && actionNames.has(req.body.action)) {
-
+  if (req.body && req.body.action) {
     switch (req.body.action) {
       case 'tokenize': {
-        ok.body = JSON.stringify(actions.tokenize(req.body.text));
+        ok.body = JSON.stringify(tokenize(req.body.text, req.body.tokenizer || 'WordTokenizer'));
+        break;
+      }
+      case 'stem': {
+        ok.body = JSON.stringify(stem(req.body.text, req.body.stemmer || 'PorterStemmer'));
         break;
       }
       case 'match': {
-        ok.body = JSON.stringify(actions.match(req.body.text1, req.body.text2));
+        ok.body = JSON.stringify(match(req.body.text1, req.body.text2));
         break;
       }
       case 'distance': {
-        ok.body = JSON.stringify(actions.distance(req.body.text1, req.body.text2, req.body.metric || 'LevenshteinDistance'));
+        ok.body = JSON.stringify(distance(req.body.text1, req.body.text2, req.body.metric || 'LevenshteinDistance'));
         break;
       }
       case 'sentiment': {
-        ok.body = JSON.stringify(actions.sentiment(req.body.text));
+        ok.body = JSON.stringify(sentiment(req.body.text));
         break;
       }
       case 'tokenizeAndStem': {
-        ok.body = JSON.stringify(actions.tokenizeAndStem(req.body.text));
+        ok.body = JSON.stringify(tokenizeAndStem(req.body.text,  req.body.tokenizer || 'WordTokenizer', req.body.stemmer || 'PorterStemmer'));
         break;
       }
       case 'spellcheck': {
-        ok.body = JSON.stringify(actions.spellcheck(req.body.text));
+        ok.body = JSON.stringify(spellcheck(req.body.text));
         break;
       }
       default:
         context.res = bad;
         return;
     }
-
     context.res = ok;
 
   } else {
